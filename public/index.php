44911a0f7fce6a4204a8b77ecd2038f9dbbf17f5
Agustin Sanchez <sanchezfabrega@gmail.com>
2018-03-17 11:52:29 +0100

    primer commit

---
+++ public/index.php
@@ -0,0 +1,41 @@
+<?php
+
+require '../vendor/autoload.php';
+
+//$element = new \App\HtmlElement();
+//echo $element->render();;
+
+$element = new \App\HtmlElement('p', [], 'Este es el contenido');
+
+
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new \App\HtmlElement('p', ['id' => 'my_paragraph'], 'Este es el contenido');
+
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new \App\HtmlElement('p', ['id' => 'my_paragraph', 'class' => 'paragraph'], 'Este es el contenido');
+
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new \App\HtmlElement('img', ['src' => 'img/styde.png']);
+
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new \App\HtmlElement('img', ['src' => 'img/styde.png', 'title' => 'Curso de "Refactorización" en Styde']);
+
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new \App\HtmlElement('input', ['required']);
+
+echo htmlentities($element->render(), ENT_QUOTES, 'UTF-8'); 
