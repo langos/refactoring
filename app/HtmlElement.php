4ebeca6a27e23342a6aa7a44658e0719d6dcfbfe
Agustin Sanchez <sanchezfabrega@gmail.com>
2018-03-21 12:50:23 +0100

    Extraer clase

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -20,7 +20,7 @@ class HtmlElement
     public function __construct(string $name, array $attributes = [], $content = null)
     {
         $this->name = $name;
-        $this->attributes = $attributes;
+        $this->attributes = new HtmlAttributes($attributes); 
         $this->content = $content;
     }
 
@@ -35,34 +35,12 @@ class HtmlElement
 
     public function open(): string
     {
-        if ($this->hasAttributes()) {
-            return '<'.$this->name.$this->attributes().'>';
-        } else {
-            return '<'.$this->name.'>';
-        }
+        return '<'.$this->name.$this->attributes().'>';
     }
 
-    public function hasAttributes(): bool
+    public function attributes()
     {
-        return ! empty($this->attributes);
-    }
-
-    public function attributes(): string
-    {
-        // $htmlAttributes
-        return array_reduce(array_keys($this->attributes), function ($result, $attribute) {
-            return $result . $this->renderAttribute($attribute);
-        }, '');
-    
-    }
-
-    protected function renderAttribute($attribute)
-    {
-        if (is_numeric($attribute)) {
-            return ' '.$this->attributes[$attribute];
-        }
-
-        return ' '.$attribute.'="'.htmlentities($this->attributes[$attribute], ENT_QUOTES, 'UTF-8').'"'; // name="value"
+        return $this->attributes->render();
     }
 
     public function isVoid(): bool
